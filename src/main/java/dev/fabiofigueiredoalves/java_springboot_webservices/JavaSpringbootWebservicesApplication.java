a02573d06fce146bbd001e1da5b193a9bed4739a
Fabio Alves <contato.figueiredofabio@gmail.com>
2025-08-08 15:40:46 -0300

    Project created

---
+++ src/main/java/dev/fabiofigueiredoalves/java_springboot_webservices/JavaSpringbootWebservicesApplication.java
@@ -0,0 +1,13 @@
+package dev.fabiofigueiredoalves.java_springboot_webservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringbootWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringbootWebservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/fabiofigueiredoalves/java_springboot_webservices/JavaSpringbootWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.fabiofigueiredoalves.java_springboot_webservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringbootWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
