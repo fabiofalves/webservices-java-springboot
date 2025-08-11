1ec2cecc80bdc8c3673dbda2bef11984b93722df
Fabio Alves <contato.figueiredofabio@gmail.com>
2025-08-11 14:35:01 -0300

    OrderStatus enum

--- src/main/java/dev/fabiofigueiredoalves/java_springboot_webservices/entities/Order.java
+++ src/main/java/dev/fabiofigueiredoalves/java_springboot_webservices/entities/Order.java
@@ -1,6 +1,7 @@
 package dev.fabiofigueiredoalves.java_springboot_webservices.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import dev.fabiofigueiredoalves.java_springboot_webservices.entities.enums.OrderStatus;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -28,12 +29,15 @@ public class Order implements Serializable {
     @JoinColumn(name = "client_id")
     private User client;
 
+    private Integer orderStatus;
+
     public Order(){
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -51,6 +55,16 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null){
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public User getClient() {
